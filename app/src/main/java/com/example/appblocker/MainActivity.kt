45f70cd7659f59b084585ecd5469394e00af3c86
Intern01 <moizz1326@gmail.com>
2025-07-21 12:50:29 +0500

    fin

--- app/src/main/java/com/example/appblocker/MainActivity.kt
+++ app/src/main/java/com/example/appblocker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.appblocker
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
@@ -12,11 +13,12 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        window.statusBarColor = Color.TRANSPARENT
         setContentView(R.layout.activity_main)
 
         if (!Settings.canDrawOverlays(this)) {
-            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+            val intent =
+                Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
             startActivity(intent)
         }
         startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
--- app/src/main/java/com/example/appblocker/Service.kt
+++ app/src/main/java/com/example/appblocker/Service.kt
@@ -4,34 +4,110 @@ import android.accessibilityservice.AccessibilityService
 import android.graphics.PixelFormat
 import android.graphics.Rect
 import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.WindowInsets
 import android.view.WindowManager
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class AppLoggerService : AccessibilityService() {
 
     private var windowManager: WindowManager? = null
     private var overlayView: View? = null
+    private var isDialerBlocked = false
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-    if (event?.source == null) return
 
-    val rootNode = rootInActiveWindow ?: return
-    val reelRecycler = rootNode.findAccessibilityNodeInfosByViewId(
-        "com.google.android.youtube:id/reel_recycler"
-    )
+        if (event?.source == null) return
 
-    if (reelRecycler.isNotEmpty()) {
-        Log.d("YTShortsDetector", "✅ YouTube Shorts is OPEN")
-        showBlockOverlay()
-    } else {
-        Log.d("YTShortsDetector", "⛔ Shorts not detected")
+        val rootNode = rootInActiveWindow ?: return
+        Log.d("AppLogger", "Event: ${rootNode.packageName}")
+        val reelRecycler = rootNode.findAccessibilityNodeInfosByViewId(
+            "com.google.android.youtube:id/reel_recycler"
+        )
+
+        if (rootNode.packageName.contains("com.sh.smart.caller")) {
+            showDialerBlocker()
+            Log.d("AppLogger", "Dialer Detected")
+        }
+        if (reelRecycler.isNotEmpty()) {
+            Log.d("YTShortsDetector", "✅ YouTube Shorts is OPEN")
+            showBlockOverlay()
+        } else {
+            Log.d("YTShortsDetector", "⛔ Shorts not detected")
+        }
     }
+
+    private fun showDialerBlocker() {
+        if (overlayView != null || isDialerBlocked) return
+        isDialerBlocked = true
+        Handler(Looper.getMainLooper()).postDelayed({ isDialerBlocked = false }, 10000)
+
+        val layoutParams = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.MATCH_PARENT,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+            else
+                WindowManager.LayoutParams.TYPE_PHONE,
+            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
+                    WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM,
+            PixelFormat.TRANSLUCENT
+        )
+
+        val inflater = LayoutInflater.from(this)
+        overlayView = inflater.inflate(R.layout.block_dialer_overlay, null)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val controller = overlayView?.windowInsetsController
+            controller?.show(WindowInsets.Type.navigationBars() or WindowInsets.Type.statusBars())
+        }
+
+        val pin = overlayView?.findViewById<EditText>(R.id.pin)
+        val submit = overlayView?.findViewById<Button>(R.id.submit_btn)
+        val back= overlayView?.findViewById<ImageView>(R.id.backspace)
+        val buttonIds = listOf(
+            R.id.one, R.id.two, R.id.three,
+            R.id.four, R.id.five, R.id.six,
+            R.id.seven, R.id.eight, R.id.nine
+        )
+        back?.setOnClickListener {
+            val text = pin?.text
+            if (!text.isNullOrEmpty()) {
+                text.delete(text.length - 1, text.length)
+            }
+        }
+
+        for (id in buttonIds) {
+            overlayView?.findViewById<AppCompatButton>(id)?.setOnClickListener { button ->
+                val digit = (button as AppCompatButton).text.toString()
+                pin?.append(digit)
+            }
+        }
+        submit?.setOnClickListener {
+            if (pin?.text.toString() == "1234") {
+                performGlobalAction(GLOBAL_ACTION_BACK)
+                windowManager?.removeView(overlayView)
+                overlayView = null
+            } else {
+                pin?.text?.clear()
+            }
+        }
+
+
+        windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
+        windowManager?.addView(overlayView, layoutParams)
     }
 
 
@@ -64,12 +140,12 @@ class AppLoggerService : AccessibilityService() {
         windowManager?.addView(overlayView, layoutParams)
     }
 
-
     override fun onInterrupt() {}
 
     override fun onServiceConnected() {
         Log.d("AppLogger", "Accessibility Service Connected")
     }
+
     private fun containsShortsKeyword(node: AccessibilityNodeInfo?): Boolean {
         if (node == null) return false
 
